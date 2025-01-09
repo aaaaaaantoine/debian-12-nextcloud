d634b36c00ddaccebc6f6adc2bc7777c1b116b03
aaaaaaantoine <antoine.ducroux3@gmail.com>
2025-01-09 05:48:39 +0100

    Update

---
+++ debian-lamp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Mises à jour du système
+sudo apt update && sudo apt full-upgrade -y
+
+## LAMP installation 
+sudo apt install apache2 mariadb-server php php-gd php-mbstring php-xml php-zip php-curl php-mysql -y
+
+## Système
+sudo systemctl enable --now apache2 mariadb
+sudo mysql_secure_installation
