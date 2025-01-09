0472dc55c40f08c2f1df88399aeb196cd6e0bd7b
aaaaaaantoine <antoine.ducroux3@gmail.com>
2025-01-09 12:52:14 +0100

    Update debian-lamp.sh

--- debian-lamp.sh
+++ debian-lamp.sh
@@ -3,7 +3,7 @@
 ## Mises à jour du système
 sudo apt update && sudo apt full-upgrade -y
 
-## LAMP installation 
+## LAMP installation (PHP Nextcloud)
 sudo apt install apache2 mariadb-server php php-gd php-mbstring php-xml php-zip php-curl php-mysql -y
 
 ## Système
