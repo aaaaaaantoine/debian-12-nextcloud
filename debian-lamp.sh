ddacf01d9782787e3feb0993acc489af8bfdf544
aaaaaaantoine <antoine.ducroux3@gmail.com>
2025-01-14 08:51:48 +0100

    Delete debian-lamp.sh

--- debian-lamp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt full-upgrade -y
-sudo apt install apache2 mariadb-server php php-gd php-mbstring php-xml php-zip php-curl php-mysql -y
-sudo systemctl enable --now apache2 mariadb
-sudo mysql_secure_installation
